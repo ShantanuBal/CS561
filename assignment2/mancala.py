fd5ecda3eafd4acf24f7942c4b915bbd302b0a87
ShantanuBal <shantanu.r.bal@gmail.com>
2015-10-03 23:11:37 -0700

    Adding new mancala

--- assignment2/mancala.py
+++ assignment2/mancala.py
@@ -1,149 +1,78 @@
 import sys
 import pdb
 #pdb.set_trace()
-current = 0
 
-def find_nodes(line):
-	node_list = []
-	node = ""
-	for i in range(len(line)):
-		if line[i] == " ":
-			node_list.append(node)
-			node = ""
-		elif line[i] not in ['\n','\r']:
-			node += line[i]
-	node_list.append(node)
-	return node_list
-
-def find_off(line):
-	time_list = []
-	for each in line:
-		time = []
-		for every in each.split("-"):
-			time.append(int(every)%24)
-		time_list.append(time)
-	return time_list
-
-# insert node in the right position of the open queue
-def insert_node(queue, node, time):
-	"""
-	for i in range(len(queue)):
-		if node == queue[i][0]:
-			if time < queue[i][1]:
-				queue.pop(i)
-				break
-			else:
-				return queue
-	"""
-	for i in range(len(queue)):
-		if time < queue[i][1] or (time == queue[i][1] and node < queue[i][0]):
-			queue = queue[:i] + [[node, time]] + queue[i:]
-			break
-	else:
-		queue.append([node, time])
-	return queue
-
-# check if pipe is usable at this point of time
-def isPipeActive(time, timeOffList):
-	time = time%24
-	for each in timeOffList:
-		if each[0] <= time <= each[1] or (each[1] < each[0] and (each[0] <= time or time <= each[1])):
-			return False
-	return True
-
-def UCS(src, des, mid, edg, stt):
-	if current == 18:
-		pdb.set_trace()
-	open_queue = [[src, stt]]
-	visited = {}
-	while open_queue:
-		top = open_queue.pop(0)
-		node, time = top[0], top[1]
-		if node in des:
-			return str(node) + " " + str(time%24)
-		if node in visited:
-			continue
-		visited[node] = True
-		if node in edg:
-			for edge in edg[node]:
-				if isPipeActive(time, edge['off']):
-					open_queue = insert_node(open_queue, edge['to'], time+edge['len'])
-				else:
-					del visited[node]
-	return "None"
-
-def DFS(src, des, mid, edg, stt):
-	stack = [[src, stt]]
-	visited = {}
-	while stack:
-		top = stack.pop()
-		node, time = top[0], top[1]
-		if node in des:
-			return str(node) + " " + str(time%24)
-		if node in visited:
-			continue
-		visited[node] = True
-		if node in edg:
-			for edge in edg[node][::-1]:
-				stack.append([edge['to'], (time+1)])
-
-	return "None"	
-
-
-def BFS(src, des, mid, edg, stt):
-	queue = [[src, stt]]
-	visited = {}
-	while queue:
-		top = queue.pop(0)
-		node, time = top[0], top[1]
-		if node in des:
-			return str(node) + " " + str(time%24)
-		if node in visited:
-			continue
-		visited[node] = True
-		if node in edg:
-			for edge in edg[node]:
-				queue.append([edge['to'], (time+1)])
-
-	return "None"
-
-f = open(sys.argv[2], "r")
-fout = open("output.txt", "w")
-cases = int(f.readline())
-while current < cases:
-	alg = f.readline()[:3]
-	line = f.readline()
-	src = ""
-	for each in line:
-		if each in ['\n','\r']:
-			break
+def getBoard(string):
+	board = []
+	value = ""
+	for each in string:
+		if each == " ":
+			board.append(int(value))
+			value = ""
 		else:
-			src += each
-	des = find_nodes(f.readline())
-	mid = find_nodes(f.readline())
-	pip = int(f.readline())
-	edg = {}
-	for i in range(pip):
-		pip_info = find_nodes(f.readline())
-		off = find_off(pip_info[4:])
-		if pip_info[0] not in edg:
-			edg[pip_info[0]] = []
-		new_edge = {'to':pip_info[1], 'len':int(pip_info[2]), 'off':off}
-		edg[pip_info[0]].append(new_edge)
+			value += each
+	return board + [int(value)]
 	
-	for each in edg:
-		edg[each].sort(key=lambda node:node['to'])
+f = open(sys.argv[2], "r")
+fout = open("next_state.txt", "w")
 	
-	stt = int(f.readline())
-	empty = f.readline()
-	
-	if alg == "BFS":
-		result = BFS(src, des, mid, edg, stt)
-	elif alg == "DFS":
-		result = DFS(src, des, mid, edg, stt)
-	else:
-		result = UCS(src, des, mid, edg, stt)
-	fout.write(result + "\n")
-	current += 1
+task = int(f.readline())
+player = int(f.readline())
+cutoff = int(f.readline())
+board2= getBoard(f.readline())
+board1 = getBoard(f.readline())
+mancala2 = int(f.readline())
+mancala1 = int(f.readline())
+
+def greedy(board2, board1, player, level, cutoff, mancala2, mancala1, n):
+	if level > cutoff:
+		return mancala1 - mancala2
+	max_index = -1
+	max_val = -1000
+	for i in range(board1):
+		new_board2 = board2
+		new_board1 = board1
+		stones = new_board1[i]
+		new_board1[i] = 0
+		quo = stone / (2*n + 2)
+		rem = stone % (2*n + 2)
+		# make this dynamic
+		new_player = 2
+
+		for j in range(n):
+			new_board2[j] += quo
+			new_board1[j] += quo
+		j = i+1
+		while rem and j < n:
+			new_board1[j] += 1
+			rem -= 1
+			j += 1
+			if not rem and new_board1[j-1] == 1:
+				new_player = 1
+		if rem:
+			mancala1 += 1
+			rem -= 1
+			if not rem:
+				new_player = 1
+
+		j = n-1
+		while rem and j >= 0:
+			new_board2[j] += 1
+			j -= 1
+			rem -= 1
+		j = 0
+		while rem and j < i:
+			new_board1[j] += 1
+			rem -= 1
+			j += 1
+			
+
+if task == 1:
+	greedy(board2, board1, player, 0, cutoff, mancala2, mancala1, len(board2))
+elif task == 2:
+	miniMax()
+elif task == 3:
+	alphaBeta()
+
 f.close()
 fout.close()
